8261ec1b767fb5029823c4a85100b41c43834baf
Rafał Sokulski <rsokulski@op.pl>
2025-01-02 13:05:10 +0100

    Add missing test cases

--- test/UserDetails.ts
+++ test/UserDetails.ts
@@ -1,10 +1,32 @@
 import hre from 'hardhat';
 import {expect} from 'chai';
+import {UserDetails} from '../typechain-types';
 
 describe('UserDetails', () => {
-  it('Should deploy UserDetails contract ', async () => {
-    const userDetails = await hre.ethers.deployContract('UserDetails');
 
-    expect(await userDetails.registeredUsers()).to.equal(0);
+  let userDetailsContract: UserDetails;
+
+  beforeEach(async () => {
+    userDetailsContract = await hre.ethers.deployContract('UserDetails');
   });
+
+  it('Should deploy UserDetails contract ', async () => {
+    expect(await userDetailsContract.registeredUsers()).to.equal(0);
+  });
+
+  it('Should register new user', async () => {
+    await expect(userDetailsContract.register('Rafał', 'Sokulski', 18))
+      .to
+      .emit(userDetailsContract, 'UserRegistered')
+      .withArgs('0xbD004d9048C9b9e5C4B5109c68dd569A65c47CF9');
+
+    expect(await userDetailsContract.registeredUsers()).to.equal(1);
+
+    expect((await userDetailsContract.getDetails())['0']).to.equal('Rafał');
+    expect((await userDetailsContract.getDetails())['1']).to.equal('Sokulski');
+    expect((await userDetailsContract.getDetails())['2']).to.equal('0xbD004d9048C9b9e5C4B5109c68dd569A65c47CF9');
+    expect((await userDetailsContract.getDetails())['3']).to.equal(18);
+  });
+
+
 });
