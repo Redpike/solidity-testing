b19984262b59225abe603676c72292383780332b
Rafał Sokulski <rsokulski@op.pl>
2025-01-02 13:18:55 +0100

    Add missing test cases

--- test/UserDetails.ts
+++ test/UserDetails.ts
@@ -26,7 +26,29 @@ describe('UserDetails', () => {
     expect((await userDetailsContract.getDetails())['1']).to.equal('Sokulski');
     expect((await userDetailsContract.getDetails())['2']).to.equal('0xbD004d9048C9b9e5C4B5109c68dd569A65c47CF9');
     expect((await userDetailsContract.getDetails())['3']).to.equal(18);
+
+    // Cleanup
+    await expect(userDetailsContract.unregister('0xbD004d9048C9b9e5C4B5109c68dd569A65c47CF9'))
+      .to
+      .emit(userDetailsContract, 'UserDeleted')
+      .withArgs('0xbD004d9048C9b9e5C4B5109c68dd569A65c47CF9');
+
+    expect(await userDetailsContract.registeredUsers()).to.equal(0);
   });
 
+  it('Try to register the same user', async () => {
+    await expect(userDetailsContract.register('Rafał', 'Sokulski', 18))
+      .to
+      .emit(userDetailsContract, 'UserRegistered')
+      .withArgs('0xbD004d9048C9b9e5C4B5109c68dd569A65c47CF9');
 
+    expect(await userDetailsContract.registeredUsers()).to.equal(1);
+
+    await expect(userDetailsContract.register('Rafał', 'Sokulski', 18))
+      .to
+      .be
+      .rejectedWith('Already registered');
+
+    expect(await userDetailsContract.registeredUsers()).to.equal(1);
+  });
 });
